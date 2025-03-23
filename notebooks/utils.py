4b0b265bdcd3ac761f624d209003ccb085d7cbea
victornemenike <emenikevn@yahoo.com>
2025-03-23 22:10:22 +0100

    quantile normalization of peaks

---
+++ notebooks/utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+def quantile_normalize(df):
+    rank_mean = df.stack().groupby(df.rank(method='first').stack().astype(int)).mean()
+    return df.rank(method='min').stack().astype(int).map(rank_mean).unstack()
