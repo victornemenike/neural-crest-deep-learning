387bcb25d1a0249d4def852bd0c850cffb985d58
victornemenike <emenikevn@yahoo.com>
2025-04-22 15:44:37 +0200

    update quantilze_normalize function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,5 +2,8 @@ import numpy as np
 import pandas as pd
 
 def quantile_normalize(df):
-    rank_mean = df.stack().groupby(df.rank(method='first').stack().astype(int)).mean()
-    return df.rank(method='min').stack().astype(int).map(rank_mean).unstack()
+    sorted_df = np.sort(df.values, axis=0)
+    mean_sorted = np.mean(sorted_df, axis=1)
+    ranks = np.argsort(np.argsort(df.values, axis=0), axis=0)
+    normalized = mean_sorted[ranks]
+    return pd.DataFrame(normalized, columns=df.columns, index=df.index)
